42a426fd56aedd328cb39454d312d38a10296ca0
mxou <maximilien.dlrm@gmail.com>
2024-05-20 05:26:06 +0200

    Fini pour la fonctionnalité de limiter le nombre de joueurs au minimum de 2

--- assets/js/front.js
+++ assets/js/front.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const remove_btn = document.querySelector('.remove_btn');
     const add_btn = document.querySelector('.add_btn');
     const count_btn = document.querySelector('.count_btn');
-    let count_player = 2;
+    let count_player = 3;
     count_btn.textContent = Number(count_player);
     console.log(count_btn.textContent);
 
@@ -15,28 +15,32 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
+    function updateButtonState() {
+    if (count_player === 2) {
+        remove_btn.style.backgroundColor = '#FCD772';
+        remove_btn.disabled = true;
+        remove_btn.style.pointerEvents = 'none';
+    } else {
+        remove_btn.style.backgroundColor = '#FF8811';
+        remove_btn.disabled = false;
+        remove_btn.style.pointerEvents = 'auto';
+    }
+}
+
     
 
     add_btn.addEventListener('click', function(){
         count_player = count_player + 1;
         count_btn.textContent = Number(count_player);
         console.log(count_player);
-         if (count_player === 2) {
-            remove_btn.style.backgroundColor = '#FCD772';
-        } else { 
-            remove_btn.style.backgroundColor = '#FF8811';
-        }
+        updateButtonState();
     });
 
      remove_btn.addEventListener('click', function(){
         count_player = count_player - 1;
         count_btn.textContent = Number(count_player);
         console.log(count_player);
-        if (count_player === 2) {
-            remove_btn.style.backgroundColor = '#FCD772';
-        } else { 
-            remove_btn.style.backgroundColor = '#FF8811';
-        }
+        updateButtonState();
     });
 
 
