f928e1539f37dc916d99c0810d6383f0d73eac13
DELORME MAXIMILIEN <mdelor03@iut.local>
2024-06-05 10:12:24 +0200

    retirer le head de mort du index

--- index.php
+++ index.php
@@ -2,12 +2,6 @@
 include('./assets/php/functions.php'); 
 echo head('TITRE'); ?>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="assets/img/Guesswhatalpha@2X.png" />
-  <link rel="stylesheet" href="./assets/styles/main.css">
-  <title>$title</title>
-</head>
   <body>
     <div class="element_container">
       <img
