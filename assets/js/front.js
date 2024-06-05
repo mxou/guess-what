f68e59aea4f65132681a74523637b14596c72912
AMIOT FLORIAN <famiot@iut.local>
2024-06-05 11:36:48 +0200

    totu bon

--- assets/js/front.js
+++ assets/js/front.js
@@ -1,4 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+
+    const count_page_btn = document.querySelectorAll('.count_page_btn');
+    const remove_btn = document.querySelector('.remove_btn');
+    const add_btn = document.querySelector('.add_btn');
+    const random_btn = document.querySelectorAll('.button')
+    const arbutton = document.querySelectorAll('.add_remove-btn')
 
     gsap.from(".buttonIndex", {
         opacity: 0,
@@ -34,50 +40,112 @@ document.addEventListener('DOMContentLoaded', function() {
         ease: "power2.inOut"
     }, "-=30%");
 
+    // random_btn.addEventListener('click', function () {
+    //     gsap.fromTo('.button', {
+    //         scale: .8,
+    //         duration: .2,
+    //         ease: "power2.out"
+    //     })
+    // })
+    // arbutton.addEventListener('click', function () {
+    //     gsap.fromTo('.add_remove_btn', {
+    //         scale: .8,
+    //         duration: .2,
+    //         ease: "power2.out"
+    //     })
+    //     console.log('écrase toi')
+    // })
 
-    const count_page_btn = document.querySelectorAll('.count_page_btn');
-    const remove_btn = document.querySelector('.remove_btn');
-    const add_btn = document.querySelector('.add_btn');
+    gsap.from('h1', {
+        y: -100,
+        opacity: 0,
+        duration: 1.3,
+        ease: "elastic.out"
+    })
+
+    gsap.from('.button:not(.buttonIndex)', {
+        x: -200,
+        opacity: 0,
+        stagger: .2,
+        duration: .5,
+        ease: "power2.out"
+    })
+
+    // gsap.from('add_remove_button', {
+    //     opacity: 0,
+    //     duration: .3,
+    //     delay: .2,
+    //     ease: "power2.out"
+    // })
+
     const count_btn = document.querySelector('.count_btn');
     let count_player = 3;
     count_btn.textContent = Number(count_player);
     console.log(count_btn.textContent);
 
-    count_page_btn.forEach(function(button) {
-        button.addEventListener('click', function() {
+    count_page_btn.forEach(function (button) {
+        button.addEventListener('click', function () {
             this.classList.toggle('button_active');
             console.log('Classe togglée');
         });
     });
 
     function updateButtonState() {
-    if (count_player === 2) {
-        remove_btn.style.backgroundColor = '#FCD772';
-        remove_btn.disabled = true;
-        remove_btn.style.pointerEvents = 'none';
-    } else {
-        remove_btn.style.backgroundColor = '#FF8811';
-        remove_btn.disabled = false;
-        remove_btn.style.pointerEvents = 'auto';
+        if (count_player === 2) {
+            remove_btn.style.backgroundColor = '#FCD772';
+            remove_btn.disabled = true;
+            remove_btn.style.pointerEvents = 'none';
+        } else {
+            remove_btn.style.backgroundColor = '#FF8811';
+            remove_btn.disabled = false;
+            remove_btn.style.pointerEvents = 'auto';
+        }
+        playersInput.value = count_player;
     }
-    playersInput.value = count_player;
-}
 
-    
 
-    add_btn.addEventListener('click', function(){
+
+    add_btn.addEventListener('mousedown', function () {
+        gsap.to('.add_btn', {
+            scale: 0.5,
+            duration: 0.1,
+            ease: "power2.out"
+        });
+    });
+
+    add_btn.addEventListener('mouseup', function () {
         count_player = count_player + 1;
         count_btn.textContent = Number(count_player);
+        gsap.to('.add_btn', {
+            scale: 1,
+            duration: 0.1,
+            ease: "power2.out"
+        });
         console.log(count_player);
         updateButtonState();
     });
 
-     remove_btn.addEventListener('click', function(){
+
+    remove_btn.addEventListener('mousedown', function () {
+        gsap.to('.remove_btn', {
+            scale: 0.5,
+            duration: 0.1,
+            ease: "power2.out"
+        });
+    });
+
+    remove_btn.addEventListener('mouseup', function () {
         count_player = count_player - 1;
         count_btn.textContent = Number(count_player);
+        gsap.to('.remove_btn', {
+            scale: 1,
+            duration: 0.1,
+            ease: "power2.out"
+        });
         console.log(count_player);
         updateButtonState();
     });
+
 
 
 
