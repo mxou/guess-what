52fd9e5b227f84364aa1312bb64c36ec29350056
mxou <maximilien.dlrm@gmail.com>
2024-06-19 22:25:54 +0200

    fix de la variable de session pour les joueurs, leur ordre de passage, et le session destroy

--- theme_choice.php
+++ theme_choice.php
@@ -10,19 +10,37 @@ include('./assets/php/functions.php');
 echo head('TITRE'); 
 
 $player_names = sessionPlayers();
-// var_dump($player_names);
+if (!empty($player_names)) {
+    for ($i = 0; $i < count($player_names); $i++) {
+        var_dump(htmlspecialchars($player_names[$i]));
+    }
+} else {
+    echo "Aucun joueur trouvé.";
+}
+
+if (isset($_POST['reset_session'])) {
+    session_destroy();
+    header('Location: player_count.php');
+    exit;
+}
 
 ?>
 
 <body class="tc">
-<div id="bubble-container"></div>
+    <div id="bubble-container"></div>
     <h1>Choix du theme</h1>
     <form action="pregame.php" method="get" class="theme_container">
-        <button type="submit" name="theme" class="theme" value="films"> <img src="./assets/img/films.png" alt=""><span>Films</span></button>
-        <button type="submit" name="theme" class="theme" value="acteurs"><img src="./assets/img/acteurs.png" alt=""><span>Acteurs</span></button>
-        <button type="submit" name="theme" class="theme" value="musiques"><img src="./assets/img/musique.png" alt=""><span>Musique</span></button>
-        <button type="submit" name="theme" class="theme" value="jeux"><img src="./assets/img/jeux.png" alt=""><span>Jeux</span></button>
+        <button type="submit" name="theme" class="theme" value="films"> <img src="./assets/img/films.png"
+                alt=""><span>Films</span></button>
+        <button type="submit" name="theme" class="theme" value="acteurs"><img src="./assets/img/acteurs.png"
+                alt=""><span>Acteurs</span></button>
+        <button type="submit" name="theme" class="theme" value="musiques"><img src="./assets/img/musique.png"
+                alt=""><span>Musique</span></button>
+        <button type="submit" name="theme" class="theme" value="jeux"><img src="./assets/img/jeux.png"
+                alt=""><span>Jeux</span></button>
     </form>
-    <a href="./player_count.php" class="button count_page_btn">Retour</a>
+    <form method="post" action="">
+        <button type="submit" name="reset_session" class="button count_page_btn">Retour</button>
+    </form>
     <?php echo scripts(); ?>
 </body>
